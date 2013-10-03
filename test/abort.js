5e8168e2f34ce4e7dfa72807021836d8a52256ad
Jason Frame <jason@onehackoranother.com>
2013-10-03 13:54:21 +0100

    more thorough testing

--- test/abort.js
+++ test/abort.js
@@ -2,13 +2,17 @@ var test = require('tape');
 var esprima = require('esprima');
 var codegen = require('escodegen');
 var slowpoke = require('../');
-
+var util = require('util');
 
 function ta(msg, code) {
 
     test(msg, function(assert) {
 
+        console.log(msg);
+
         var ast = esprima.parse(code);
+
+        // console.log(util.inspect(ast, {depth: null, colors: true}));
 
         slowpoke(ast, {timeout: 2000});
 
@@ -20,6 +24,7 @@ function ta(msg, code) {
             assert.fail();
             assert.ok(Date.now() - now >= 2000);
         } catch (e) {
+            console.log(e.message);
             assert.pass();
         }
 
@@ -29,7 +34,27 @@ function ta(msg, code) {
 
 }
 
-ta('while loop aborts', 'while (true) {}');
-ta('nested while loop aborts', 'var i = 0; while (i < 10) { while (true) {}; i++; }');
-ta('for loop aborts', 'for (var i = 0; i < 1; i = i) {}');
-ta('do loop aborts', 'do {} while (true)');
+var cases = {
+    'while - empty'             : 'while (true);',
+    'while - empty block'       : 'while (true) {}',
+    'while - single statement'  : 'var x = 0; while (true) x++;',
+    'while - multi statement'   : 'var x = 0, y = 0; while (true) { x++; y++; }',
+
+    'for - empty'               : 'for (var i = 0; i < 1; i = i);',
+    'for - empty block'         : 'for (var i = 0; i < 1; i = i) {}',
+    'for - single statement'    : 'var x = 0; for (var i = 0; i < 1; i = i) x++;',
+    'for - multi statement'     : 'var x = 0, y = 0; for (var i = 0; i < 1; i = i) { x++; y++; }',
+
+    'do - empty'                : 'do ; while (true)',
+    'do - empty block'          : 'do {} while (true)',
+    'do - single statement'     : 'var x = 0; do x++; while (true)',
+    'do - multi statement'      : 'var x = 0, y = 0; do { x++; y++; } while (true)'
+};
+
+for (var k in cases) {
+    ta(k, cases[k]);
+}
+
+for (var k in cases) {
+    ta(k + ' (wrapped)', '(function() { ' + cases[k] + ' })()');
+}
